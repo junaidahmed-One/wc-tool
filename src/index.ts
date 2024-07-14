036e3b6471db8c1a9506dd74e1710c7e2d13b563
junaidahmed-One <junaidahmed882000@gmail.com>
2024-07-14 20:24:40 +0530

    step Two completed

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
-import { numberOfbytes } from "./util";
+import { numberOfbytes, numberOfLines } from "./util";
 import path from "path";
 
 program
 	.version("1.0.0")
 	.description("test wc")
 	.option("-n, --name <name>", "your name")
-	.option("-c, --file <filePath>", "Path to the file", numberOfbytes)
-	.action((options) => {
+	.option("-c, --byte <filePath>", "number of bytes", numberOfbytes)
+	.option("-l, --lines <filePath>", "number of lines")
+	.action(async (options) => {
 		const enteredFlags = Object.keys(options).filter(
 			(flag) => options[flag]
 		);
-
 		switch (enteredFlags[0]) {
 			case "name":
 				console.log(`Name ${options.name}`);
 				break;
-			case "file":
-				console.log(`Number of bytes in a file : ${options.file}`);
+			case "byte":
+				console.log(`Number of bytes in a file : ${options.byte}`);
+				break;
+			case "lines":
+				const line = await numberOfLines(options.lines);
+				console.log(`Number of lines : ${line}`);
 				break;
 			default:
 				console.log(
@@ -28,5 +32,11 @@ program
 		}
 		//console.log(enteredFlags);
 	});
+
+// numberOfLines("D://Cohort FullStack+DevOps/Projects/wc-tool/src/text.txt").then(
+// 	(lines) => {
+// 		console.log(lines);
+// 	}
+// );
 
 program.parse(process.argv);
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,9 @@
+import { error } from "console";
 import * as fs from "fs";
 import path from "path";
+import { types } from "util";
 
-export function numberOfbytes(filename: string) {
+function numberOfbytes(filename: string) {
 	const resolvedPath = path.resolve(filename);
 
 	if (!fs.existsSync(resolvedPath)) {
@@ -11,3 +13,21 @@ export function numberOfbytes(filename: string) {
 
 	return byteSize;
 }
+
+const read = (path: string): Promise<String> =>
+	new Promise((resolve, reject) => {
+		fs.readFile(path, "utf-8", (err, file) => {
+			if (err) reject(err);
+			resolve(file);
+		});
+	});
+
+async function numberOfLines(filename: string) {
+	const resolvedPath = path.resolve(filename);
+	const info = await read(resolvedPath);
+	//console.log(info);
+	const temp = info.split("\r\n");
+	return temp.length;
+}
+
+export { numberOfbytes, numberOfLines };
